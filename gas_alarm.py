c8cd2338e0f0990aeaad4793a7b67a03f40eb4cf
dogus ural <dogus@riddleandcode.com>
2020-08-28 14:30:21 +0200

    added scheduler class to run the task of cehcking gas price every 10 minutes

--- gas_alarm.py
+++ gas_alarm.py
@@ -2,6 +2,7 @@ import requests,json
 import smtplib
 import logging
 import traceback
+from apscheduler.schedulers.background import BackgroundScheduler
 
 class gas_station():
 
@@ -68,7 +69,7 @@ class mail_service():
         sent_from = self.gmail_user
         to = self.recipients
         subject = mail_subject
-        body = 'Ethereum Network Gas Prices : ' + '\nFastest : ' + str(ethgasstation.get_fastest_prices()) +  '\nFas : ' + str(ethgasstation.get_fast_prices()) + '\nAverage : ' + str(ethgasstation.get_average_prices()) + '\nSafe Low : ' + str(ethgasstation.get_safe_low_prices()) 
+        body = 'Ethereum Network Gas Prices : ' + '\nFastest : ' + str(gasstation.get_fastest_prices()) +  '\nFas : ' + str(gasstation.get_fast_prices()) + '\nAverage : ' + str(gasstation.get_average_prices()) + '\nSafe Low : ' + str(gasstation.get_safe_low_prices()) 
 
 
         email_text = "From: %s\nTo: %s\nSubject: %s\n\n%s" % (sent_from,", ".join(to), subject, body)
@@ -84,7 +85,30 @@ class mail_service():
             logging.error(traceback.format_exc())
 
 
-ethgasstation = gas_station()
-ethgasstation.refresh()
-gmail = mail_service()
-gmail.send(ethgasstation)
+class scheduler():
+    def __init__(self):
+        self.ethgasstation = gas_station()
+        self.gmail = mail_service()
+        self.sched = BackgroundScheduler()
+        self.configuration = system_config()
+        self.gas_price_watcher()
+        self.start_scheduler()
+    def __del__(self): 
+        self.sched.shutdown()
+    def gas_price_watcher(self):
+        self.ethgasstation.refresh()
+        if(self.configuration.get_alarm_price() >= self.ethgasstation.get_safe_low_prices()) :
+            print("Gas price of " + str(self.ethgasstation.get_safe_low_prices()) + " has triggered the alarm price of " + str(self.configuration.get_alarm_price()) + ".")
+            self.gmail.send(self.ethgasstation)
+    def start_scheduler(self):
+        self.sched.add_job(self.gas_price_watcher, 'interval', minutes = 10)
+        self.sched.start()
+    
+
+
+price_alert = scheduler()
+while True:
+    pass
+
+
+
